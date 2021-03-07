c7829ef010499ac87a8a9cea7ad7847a2a033b9b
Alex Oshiro <alex.oshiro94@gmail.com>
2021-03-07 18:41:51 -0400

    Aula 3 Alterado para realiza_calculo de imposto retornar valor

--- calculador_de_impostos.py
+++ calculador_de_impostos.py
@@ -4,7 +4,7 @@ class Calculador_de_impostos(object):
     def realiza_calculo(self, orcamento, imposto):
         imposto_calculado = imposto.calcula(orcamento)
 
-        print(imposto_calculado)
+        return imposto_calculado
 
 if __name__ == '__main__':
     from orcamento import Orcamento, Item
@@ -16,7 +16,11 @@ if __name__ == '__main__':
     orcamento.adiciona_item(Item('Item 2', 200))
     orcamento.adiciona_item(Item('Item 3', 250))
 
-    calculador.realiza_calculo(orcamento, ISS())
-    calculador.realiza_calculo(orcamento, ICMS())
-    calculador.realiza_calculo(orcamento, ICPP())
-    calculador.realiza_calculo(orcamento, IKCV())
+    iss = calculador.realiza_calculo(orcamento, ISS())
+    icms = calculador.realiza_calculo(orcamento, ICMS())
+    icpp = calculador.realiza_calculo(orcamento, ICPP())
+    ikcv = calculador.realiza_calculo(orcamento, IKCV())
+    print('ISS', iss)
+    print('ICMS', icms)
+    print('ICPP', icpp)
+    print('IKCV', ikcv)
