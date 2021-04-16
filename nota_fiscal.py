5c35cf30a1d59aee5c5f0618e55fe7f926ef608e
Alex Oshiro <alex.oshiro94@gmail.com>
2021-04-15 22:13:27 -0400

    aula 7 observer

--- nota_fiscal.py
+++ nota_fiscal.py
@@ -14,7 +14,7 @@ class Item(object):
         return self.__valor
 
 class Nota_fiscal(object):
-    def __init__(self, razao_social, cnpj, itens, data_de_emissao=date.today(), detalhes=''):
+    def __init__(self, razao_social, cnpj, itens, data_de_emissao=date.today(), detalhes='', observadores=[]):
         self.__razao_social = razao_social
         self.__cnpj = cnpj
         self.__data_de_emissao = data_de_emissao
@@ -22,6 +22,9 @@ class Nota_fiscal(object):
             raise Exception('Detalhes da nota não pode ter mais do que 20 caracteres')
         self.__detalhes = detalhes
         self.__itens = itens
+
+        for observador in observadores:
+            observador(self)
 
     @property
     def razao_social(self):
@@ -41,10 +44,16 @@ class Nota_fiscal(object):
 
 if __name__ == '__main__':
     from criador_de_nota_fiscal import Criador_de_nota_fiscal
-
+    from observadores import envia_por_email, salva_no_banco, imprime
     itens = [Item('Item A', 100), Item('Item B', 200)]
-
-    Nota_fiscal = (Criador_de_nota_fiscal()
+    nota_fiscal = Nota_fiscal(
+        cnpj='012345678901234',
+        razao_social='Oshiro Software LTDA',
+        itens=itens,
+        observadores=[envia_por_email, 
+            salva_no_banco, imprime]
+    )
+    nota_fiscal2 = (Criador_de_nota_fiscal()
                     .com_razao_social('Oshiro Software LTDA')
                     .com_cnpj('012345678901234')
                     .com_itens(itens)
